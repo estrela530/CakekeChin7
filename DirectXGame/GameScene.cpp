53868e47b421295872fbfbb4902a95aa6d20fcd7
estrela530 <daisuke.0530.otk@gmail.com>
2020-11-11 00:25:01 +0900

    .vs Debug係　すべて無視リストイキスギィ

---
+++ DirectXGame/GameScene.cpp
@@ -0,0 +1,215 @@
+﻿#include "GameScene.h"
+#include <cassert>
+
+#include "Object3d2.h"
+#include "Object3d.h"
+
+using namespace DirectX;
+
+GameScene::GameScene()
+{
+}
+
+GameScene::~GameScene()
+{
+	safe_delete(spriteBG);
+	safe_delete(object3d);
+	safe_delete(ball);
+	safe_delete(object3d2);//追加(SZK)
+}
+
+void GameScene::Initialize(DirectXCommon* dxCommon, Input* input, Audio * audio)
+{
+	// nullptrチェック
+	assert(dxCommon);
+	assert(input);
+	assert(audio);
+
+	this->dxCommon = dxCommon;
+	this->input = input;
+	this->audio = audio;
+
+	// デバッグテキスト用テクスチャ読み込み
+	if (!Sprite::LoadTexture(debugTextTexNumber, L"Resources/debugfont.png")) {
+		assert(0);
+		return ;
+	}
+	// デバッグテキスト初期化
+	debugText.Initialize(debugTextTexNumber);
+
+	// テクスチャ読み込み
+	if (!Sprite::LoadTexture(1, L"Resources/back.png")) {
+		assert(0);
+		return;
+	}
+	// 背景スプライト生成
+	spriteBG = Sprite::Create(1, { 0.0f,0.0f });
+	// 3Dオブジェクト生成
+	object3d = Object3d::Create();
+	object3d->Update();
+
+	// 3Dオブジェクト2生成(SZK) 追加
+	object3d2 = Object3d2::Create();
+	object3d2->Update();
+
+	//ボール生成
+	ball = Ball::Create();
+	ball->Update();
+}
+
+void GameScene::Update()
+{
+#pragma region //(SZK・・・・復活させた↓)
+	//(SZK・・・・復活させた↓)
+	// 現在の座標を取得
+	XMFLOAT3 position = object3d->GetPosition();
+
+	// オブジェクト移動
+	if (input->PushKey(DIK_UP) || input->PushKey(DIK_DOWN) || input->PushKey(DIK_RIGHT) || input->PushKey(DIK_LEFT))
+	{
+
+		// 移動後の座標を計算
+		if (input->PushKey(DIK_UP)) { position.y += 1.0f; }
+		else if (input->PushKey(DIK_DOWN)) { position.y -= 1.0f; }
+		if (input->PushKey(DIK_RIGHT)) { position.x += 1.0f; }
+		else if (input->PushKey(DIK_LEFT)) { position.x -= 1.0f; }
+	}
+	// 座標の変更を反映
+	object3d->SetPosition(position);
+	object3d->Update();
+	//(SZK・・・・復活させた↑)
+#pragma endregion
+
+	// カメラ移動
+	if (input->PushKey(DIK_W) || input->PushKey(DIK_S) || input->PushKey(DIK_D) || input->PushKey(DIK_A))
+	{
+		if (input->PushKey(DIK_W)) { Ball::CameraMoveVector({ 0.0f,+1.0f,0.0f }); }
+		else if (input->PushKey(DIK_S)) { Ball::CameraMoveVector({ 0.0f,-1.0f,0.0f }); }
+		if (input->PushKey(DIK_D)) { Ball::CameraMoveVector({ +1.0f,0.0f,0.0f }); }
+		else if (input->PushKey(DIK_A)) { Ball::CameraMoveVector({ -1.0f,0.0f,0.0f }); }
+	}
+
+
+	// ボール移動
+	if (input->PushKey(DIK_UP) || input->PushKey(DIK_DOWN) || input->PushKey(DIK_RIGHT) || input->PushKey(DIK_LEFT))
+	{
+		// 現在の座標を取得
+		XMFLOAT3 position = ball->GetPosition();
+
+		// 移動後の座標を計算
+		if (input->PushKey(DIK_UP)) { position.y += 1.0f; }
+		else if (input->PushKey(DIK_DOWN)) { position.y -= 1.0f; }
+		if (input->PushKey(DIK_RIGHT)) { position.x += 1.0f; }
+		else if (input->PushKey(DIK_LEFT)) { position.x -= 1.0f; }
+
+		// 座標の変更を反映
+		ball->SetPosition(position);
+	}
+#pragma region オブジェクト2の座標系
+	// オブジェクト2の座標を取得
+	XMFLOAT3 position2 = object3d2->GetPosition();
+	// オブジェクト2の座標の変更を反映
+	object3d2->SetPosition(position2);
+
+#pragma endregion
+
+#pragma region 当たり判定処理
+	//ヒット通知
+	bool hit = false;
+	//座標の差を求める
+	XMVECTOR pos_sub = XMVectorSet(
+		position.x - position2.x,
+		position.y - position2.y,
+		position.z - position2.z,
+		0);
+	//2つの距離を計算
+	pos_sub = XMVector3Length(pos_sub);
+	float dist = pos_sub.m128_f32[0];
+
+	if (dist <= object3d->radius + object3d2->radius2)
+	{
+		hit = true;
+	}
+
+	if (hit)
+	{
+		debugText.Print("Hit", 0, 0, 10);
+	}
+
+#pragma endregion
+	// 座標の変更を反映
+	object3d->SetPosition(position);
+	object3d->Update();
+	ball->Update();
+
+}
+
+void GameScene::Draw()
+{
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* cmdList = dxCommon->GetCommandList();
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(cmdList);
+	// 背景スプライト描画
+	spriteBG->Draw();
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Object3d::PreDraw(cmdList);
+
+	// 3Dオブジェクト2描画前処理
+	Object3d2::PreDraw(cmdList);
+
+	//ボール描画前処理
+	Ball::PreDraw(cmdList);
+
+	// 3Dオブクジェクトの描画
+	object3d->Draw();
+
+	// 3Dオブクジェクト2の描画
+	object3d2->Draw();
+
+	//ボールの描画
+	ball->Draw();
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+
+	// 3Dオブジェクト描画後処理
+	Object3d::PostDraw();
+
+	// 3Dオブジェクト2描画後処理
+	Object3d2::PostDraw();
+
+	//ボール描画後処理
+	Ball::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(cmdList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// デバッグテキストの描画
+	debugText.DrawAll(cmdList);
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+#pragma endregion
+}
